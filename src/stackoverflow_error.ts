38a193b2d3c911af498800212527b870748246d6
RaidasGrisk <mrraidas@gmail.com>
2022-09-26 11:25:40 +0300

    update

--- src/stackoverflow_error.ts
+++ src/stackoverflow_error.ts
@@ -1,11 +1,8 @@
 import {
-  state,
-  State,
   method,
   isReady,
   Mina,
   PrivateKey,
-  PublicKey,
   shutdown,
   AccountUpdate,
   Field,
@@ -14,22 +11,22 @@ import {
   matrixProp,
 } from 'snarkyjs';
 
-// if model weights are [12288, 128]
-// input dims are    [1, 12288]
-// import { weights } from './weights.js'
-// console.log(`model weights : [${weights.w1.length}][${weights.w1[0].length}]`)
-
+// matrix setup
+// the error is thrown only if inputDIms rows are a big number
+// runs fine if its say 64 or something similar
 let inputDims = {
-  rows: 400000,
+  rows: 64000,
   cols: 3,
 };
+
 let weightDims = {
   rows: 3,
-  cols: 400000,
+  cols: 64,
 };
 
-let createMatrix = (nRows: number, nCols: number, fill: number): Field[][] => {
-  let matrix = Array.from(Array(nRows), () => Array(nCols).fill(fill));
+// helper methods / classes
+let createMatrix = (rows: number, cols: number, fill: number): Field[][] => {
+  let matrix = Array.from(Array(rows), () => Array(cols).fill(fill));
   matrix = matrix.map((row) => row.map(Field));
   return matrix;
 };
@@ -43,21 +40,17 @@ class Input extends CircuitValue {
 }
 
 class SimpleZkapp extends SmartContract {
-  @state(PublicKey) pubKey = State<PublicKey>();
+  // if it is not @state, where will this be saved? npm package?
   w1: Field[][] = createMatrix(weightDims.rows, weightDims.cols, 1);
 
   // multiply two matrices: a and b
   matrix_mul(a: Field[][], b: Field[][]): Field[][] {
-    // assert matrices
     let [aRows, aCols] = [a.length, a[0].length];
     let [bRows, bCols] = [b.length, b[0].length];
     console.log(`
       x[${aRows}][${aCols}] *
       w[${bRows}][${bCols}] =
       [${bRows}][${bCols}]`);
-    if (!(aCols === bRows)) {
-      throw 'bad dims';
-    }
 
     // init output matrix
     /* eslint-disable no-unused-vars */
@@ -81,15 +74,8 @@ class SimpleZkapp extends SmartContract {
     return res;
   }
 
-  @method update(input: Input, pubKey: PublicKey) {
-    // matrix mult
+  @method multiply(input: Input) {
     let res = this.matrix_mul(input.value, this.w1);
-
-    // assert and update state
-    let pubKey_ = this.pubKey.get();
-    this.pubKey.assertEquals(pubKey_);
-    this.pubKey.set(pubKey);
-    console.log('FINISH METHOD');
   }
 }
 
@@ -102,11 +88,8 @@ const account = Local.testAccounts[0].privateKey;
 const zkappKey = PrivateKey.random();
 let zkappAddress = zkappKey.toPublicKey();
 
-// have to compile if using proofs?
-// https://github.com/o1-labs/snarkyjs/blob/main/src/examples/zkapps/simple_and_counter_zkapp.ts
 console.log('Compiling smart contract...');
 let { verificationKey } = await SimpleZkapp.compile();
-
 let zkapp = new SimpleZkapp(zkappAddress);
 
 console.log(`Deploying zkapp for public key ${zkappAddress.toBase58()}.`);
@@ -116,14 +99,12 @@ let tx = await Mina.transaction(account, () => {
 });
 await tx.send().wait();
 
-// https://github.com/o1-labs/snarkyjs/blob/main/src/examples/zkapps/hello_world/run.ts
-console.log('Initial State', zkapp.pubKey.get().toBase58());
-console.log(`Update state`);
+console.log(`Call the method 'multiply'`);
 let tx_ = await Mina.transaction(account, () => {
   // have to pass it as matrixprop, else will get
-  // Argument 1 of method update is not a valid circuit value
+  // Argument 1 of method multiply is not a valid circuit value
   let input_ = createMatrix(inputDims.rows, inputDims.cols, 1);
-  zkapp.update(new Input(input_), account.toPublicKey());
+  zkapp.multiply(new Input(input_));
 });
 
 // fill in the proof - this can take a while...
@@ -133,7 +114,5 @@ await tx_.prove();
 // send the transaction to the graphql endpoint
 console.log('Sending the transaction...');
 await tx_.send().wait();
-
-console.log('Updated State', zkapp.pubKey.get().toBase58());
 
 shutdown();
